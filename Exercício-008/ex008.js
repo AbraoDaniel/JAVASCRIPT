6376ec23f3e77d42e056d76ee86b7de1524c8b26
Daniel Alexsandro Abrão <101731254+AbraoDaniel@users.noreply.github.com>
2022-05-08 15:16:54 -0300

    Adicionando Exercícios

---
+++ Exercício-008/ex008.js
@@ -0,0 +1,10 @@
+var veloc = 80.1
+console.log(`A velocidade do seu carro é igual a ${veloc} km/h`)
+
+if (veloc >=80) {
+    console.log("Você está acima da velocidade permitida na via, reduza e digija com cuidado!")
+}
+
+else {
+    console.log ("Você está dentro do limite de velocidade, boa viagem!")
+} 
