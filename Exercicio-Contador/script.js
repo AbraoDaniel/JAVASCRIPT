d250fcca4bad2f3ccdd1c06ade8c5d6200d28844
Daniel Alexsandro Abrão <101731254+AbraoDaniel@users.noreply.github.com>
2022-05-09 01:53:19 -0300

    Exercício Novo

---
+++ Exercicio-Contador/script.js
@@ -0,0 +1,16 @@
+var num = 0;
+var botao = document.getElementById("contagem")
+var res = document.getElementById("resultado")
+
+function aumentar() { 
+    num++;
+    res.innerHTML = num
+}
+
+var numero = 0;
+var resul = document.getElementById("resul")
+
+function diminuir () {
+    numero++;
+    resul.innerHTML = numero
+}
